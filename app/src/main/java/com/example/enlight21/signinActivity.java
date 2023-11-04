8c5fdb57ad86fbec089a56b19b217c39f612aad5
Akshay peherkar <95123298+Akshayp02@users.noreply.github.com>
2023-11-04 13:54:17 +0530

    first

---
+++ app/src/main/java/com/example/enlight21/signinActivity.java
@@ -0,0 +1,14 @@
+package com.example.enlight21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class signinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Enlight21"
+include(":app")
